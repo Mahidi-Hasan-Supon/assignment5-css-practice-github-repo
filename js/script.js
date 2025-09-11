a99059b0f43675475ce70aaa196d0901e9d977bb
Mahidi-Hasan-Supon <mahidihasansupon@gmail.com>
2025-09-11 13:51:40 +0600

    added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let arrs =[];
+
 //function calll
 function getInputValue(value){
     const addValue = Number(document.getElementById(value).innerText)
@@ -37,6 +39,12 @@ document.getElementById('copy-btn1').addEventListener('click',function(){
     const total = copyNumber + 1;
     alert('copied the number:'+number)
     document.getElementById('copy-number').innerText = total;
+
+    arrs.push({
+        title:'National Emergency Number',
+        data: new Date().toLocaleTimeString(),
+        number:999
+    })
 })
 document.getElementById('copy-btn2').addEventListener('click',function(){
     const copyNumber  = parseInt(document.getElementById('copy-number').innerText)
@@ -44,5 +52,57 @@ document.getElementById('copy-btn2').addEventListener('click',function(){
     const total = copyNumber + 1;
     alert('copied the number:'+number)
     document.getElementById('copy-number').innerText = total;
+
+      arrs.push({
+        title:'National Emergency Number',
+        data:new Date().toLocaleTimeString(),
+        number:222
+    })
+})
+
+
+
+
+// call history btn
+
+document.getElementById('call-btn').addEventListener('click',function(){
+    const callhistorydownContainer = document.getElementById('call-history-down')
+    callhistorydownContainer.innerText=''
+    for(const arr of arrs){
+        const div = document.createElement('div')
+        div.innerHTML = `
+        <div class="gay-parent ">
+          <div>
+            <h1>${arr.title}</h1>
+            <p>${arr.number}</p>
+            
+        </div>
+        <div>
+            <h1>${arr.data}</h1>
+        </div>
+      </div>
+        `
+        callhistorydownContainer.appendChild(div)
+    }
+})
+document.getElementById('call-btn1').addEventListener('click',function(){
+    const callhistorydownContainer = document.getElementById('call-history-down')
+    callhistorydownContainer.innerText=''
+    for(const arr of arrs){
+        const div = document.createElement('div')
+        div.innerHTML = `
+        <div class="gay-parent ">
+          <div>
+            <h1>${arr.title}</h1>
+            <p>${arr.number}</p>
+            
+        </div>
+        <div>
+            <h1>${arr.data}</h1>
+        </div>
+      </div>
+        `
+        callhistorydownContainer.appendChild(div)
+    }
 })
 
